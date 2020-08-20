0cbb480a28f438edb35ff92af98b53c16764b21a
Andres <andresemm96@gmail.com>
2020-08-20 17:38:15 -0500

    Updates

---
+++ precache-manifest.c1ab47b441e1eb67788d3b6155a368c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6ece5180a76e7c9ad81c4f24ac76e4",
+    "url": "/React-app-Todos/index.html"
+  },
+  {
+    "revision": "433cf2e35d63594dad4f",
+    "url": "/React-app-Todos/static/css/main.8d7449b1.chunk.css"
+  },
+  {
+    "revision": "43a1492dcd9808d3f61e",
+    "url": "/React-app-Todos/static/js/2.a5ba9d28.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/React-app-Todos/static/js/2.a5ba9d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433cf2e35d63594dad4f",
+    "url": "/React-app-Todos/static/js/main.7ae50a87.chunk.js"
+  },
+  {
+    "revision": "77a29c189835f1f9864b",
+    "url": "/React-app-Todos/static/js/runtime-main.09796d58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-app-Todos/static/media/logo.5d5d9eef.svg"
+  }
+]);
